d69ac1942b013932c1b777720281e282015961df
Foysal Ahamed Badsha <you@example.com>
2022-05-26 18:58:49 +0600

    payment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const res = require('express/lib/response');
+const { ObjectID } = require('bson');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,17 +36,36 @@ async function run() {
         const serviceCollection = client.db('manufacture_website').collection('services');
         const bookingCollection = client.db('manufacture_website').collection('bookings');
         const userCollection = client.db('manufacture_website').collection('users');
+        const productCollection = client.db('manufacture_website').collection('products');
+
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester });
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' });
+            }
+        }
 
         app.get('/service', async (req, res) => {
             const query = {};
-            const cursor = serviceCollection.find(query);
+            const cursor = serviceCollection.find(query).project({ name: 1 });
             const services = await cursor.toArray();
             res.send(services);
         });
 
-        app.get('/user', verifyJWT, async (req, res) => {
+        app.get('/user', async (req, res) => {
             const users = await userCollection.find().toArray();
             res.send(users);
+        })
+
+        app.get('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const booking = await userCollection.findOne(query);
+            res.send(booking);
         })
 
         app.get('/admin/:email', async (req, res) => {
@@ -55,7 +75,7 @@ async function run() {
             res.send({ admin: isAdmin })
         })
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const requester = req.decoded.email;
             const requesterAccount = await userCollection.findOne({ email: requester });
@@ -84,7 +104,14 @@ async function run() {
             const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET)
             res.send({ result, AccessToken: token });
             console.log(token, process.env.ACCESS_TOKEN_SECRET)
+        });
+        app.delete('/user/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const filter = { email: email };
+            const result = await userCollection.deleteOne(filter);
+            res.send(result)
         })
+
 
         app.get('/booking', verifyJWT, async (req, res) => {
             const query = {};
@@ -98,6 +125,11 @@ async function run() {
             // const query = { customerName: customerName };
             // const bookings = await bookingCollection.find(query).toArray();
             // res.send(bookings);
+        });
+        app.post('/product', verifyJWT, verifyAdmin, async (req, res) => {
+            const product = req.body;
+            const result = await productCollection.insertOne(product);
+            res.send(result);
         })
 
         app.post('/booking', async (req, res) => {
